a4b06434f0727558bdd7fcad8144f4bb8aa2ab69
jin756 <122190478+jin756@users.noreply.github.com>
2024-06-06 17:20:05 +0800

    Add files via upload

---
+++ c2-ct.sh
@@ -0,0 +1,20 @@
+ CUDA_VISIBLE_DEVICES=1 nohup python main.py \
+--dataset AG_News \
+--log_root "./log/" \
+--data_root "./data/" \
+--trainer_name bert_ct \
+--model_name bert-base-uncased \
+--gen_val \
+--nl_batch_size 32 \
+--eval_batch_size 32 \
+--gradient_accumulation_steps 1 \
+--max_sen_len 64 \
+--lr 0.00002 \
+--num_training_steps  17500 \
+--patience 25 \
+--eval_freq 50 \
+--store_model 3 \
+--noise_level 0.4 \
+--noise_type Wmatrix_Ins  \
+--manualSeed 1234  \
+>> bert_ct-Wmatrix_Ins4.log 2>&1 &
